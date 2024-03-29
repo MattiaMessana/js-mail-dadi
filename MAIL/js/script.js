4b2257977e2ff17f2501324dcc5e16de57d1e6f0
MattiaMessana <mattia.messana@icloud.com>
2024-03-29 02:41:40 +0100

    add:finish mail project

--- MAIL/js/script.js
+++ MAIL/js/script.js
@@ -1,37 +1,39 @@
 //TEST
 // alert("ciao!");
 
-
 // 1. chiediamo all'utente la sua mail 
 // 2. controlliamo se la sua mail è presente nella lista di chi puo accedere 
 // 3. stampiamo in pagina il risultato del controllo
 
+
 // Lista delle email autorizzate
-const mailAuthorized = ["francesco.totti@gmail.com", "lucatoni@live.it", "edison.cavani@elmatador.com"] ;
+const mailAuthorized = ["francesco.totti@gmail.com", "lucatoni@live.it", "edison.cavani@elmatador.com"];
 console.log(mailAuthorized);
 
+//Individuo il bottone che mi servirà per avviare il programma
+const btn = document.getElementById("enter");
+
+
 // Chiedi all'utente la sua email
-const userMail = prompt("Inserisci la tua email:");
-console.log(userMail);
+const userMail = document.getElementById("mail");
 
-// Flag per tenere traccia dello stato di autorizzazione
-let authorized = false;
-
-// Ciclo for per controllare se l'email è autorizzata
-for (let i = 0; i < mailAuthorized.length; i++) {
-    if (userMail === mailAuthorized[i]) {
-        authorized = true;
+btn.addEventListener("click", function () {
+    // Flag per tenere traccia dello stato di autorizzazione
+    let authorized = false;
+    // Ciclo for per controllare se l'email è autorizzata
+    for (let i = 0; i < mailAuthorized.length; i++) {
+        if (userMail.value === mailAuthorized[i]) {
+            authorized = true;
+        }
     }
-};
 
-// Stampiamo  messaggio
-const reslutElem = document.getElementById("idoneo");
+    // Stampiamo  messaggio
+    const resultElem = document.getElementById("idoneo");
 
-if (authorized) {
-    reslutElem.innerHTML = "Sei idoneo allo sconto"
-    console.log(reslutElem);
-} else {
-    reslutElem.innerHTML = "Non sei idoneo allo sconto"
-    console.log(reslutElem);
-   
-};
+    if (authorized) {
+        resultElem.innerHTML = "Sei idoneo";
+    } else {
+        resultElem.innerHTML = "Non sei idoneo";
+    }
+});
+
